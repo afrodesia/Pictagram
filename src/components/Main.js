66f38d9655073abc736e859a8b75dd17027ba545
Ken Torry <kentorry@afrodesiamedia.com>
2016-12-21 22:09:26 -0600

    Created component Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import { bindActionCreators} from 'redux'
+import { connect } from 'react-redux'
+import *  as actionCreators from '../actions/actionCreators'
+import App from '../App'
+
+function mapStateToProps(state){
+	return{
+		post: state.posts,
+		comments: state.comment 
+	}
+}
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators(actionCreators, dispatch)
+}
+
+
+const Main = connect(mapStateToProps, mapDispatchToProps)(App)
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import React from 'react';
 import { render } from 'react-dom'
 
 import './index.css';
+// Components
 
-import App from './App';
+// import App from './App';
+import Main from './components/Main';
 import PhotoLayout from './components/PhotoLayout'
 import PhotoSingle from './components/PhotoSingle' 
 import { Router, Route, IndexRoute } from 'react-router'
@@ -15,7 +17,7 @@ import store, { history } from './store'
 const router = (
 	<Provider store={ store }>
 		<Router history={ history }>
-	        <Route path='/' component={App}>
+	        <Route path='/' component={Main}>
 	            <IndexRoute component={PhotoLayout}></IndexRoute>
 	            <Route path='/view/:postId' component={PhotoSingle}></Route>
 	        </Route>
